27d0c1a47c95f8ebd0ba6dffd5685124fe4b4067
Isael <iisael090@gmail.com>
2024-12-23 01:34:27 -0800

    modified:   components/Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,58 +1,54 @@
-"use client";
-
 import React from "react";
-import { Company } from "@/types"; // Import the Company type
-import { companies, testimonials } from "@/data";
-import { InfiniteMovingCards } from "./ui/InfiniteCards";
-import Image from "next/image"; // Import Next.js Image component
 
-const Clients = () => {
+import { workExperience } from "@/data";
+import { Button } from "./ui/MovingBorders";
+
+const Experience = () => {
   return (
-    <section id="testimonials" className="py-20">
+    <div className="py-20 w-full">
       <h1 className="heading">
-        Kind words from
-        <span className="text-purple"> Peers</span>
+        My <span className="text-purple">work experience</span>
       </h1>
 
-      <div className="flex flex-col items-center max-lg:mt-10">
-        <div
-          className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden"
-        >
-          <InfiniteMovingCards
-            items={testimonials}
-            direction="right"
-            speed="slow"
-          />
-        </div>
-
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
-          {companies.map((company: Company) => (
-            <React.Fragment key={company.id}>
-              <div className="flex md:max-w-60 max-w-32 gap-2">
-                {/* First Image */}
-                <Image
-                  src={company.img}
-                  alt={company.name}
-                  className="md:w-10 w-5"
-                  width={40} // Replace with actual dimensions
-                  height={40} // Replace with actual dimensions
-                />
-
-                {/* Second Image */}
-                <Image
-                  src={company.nameImg}
-                  alt={company.name}
-                  width={company.id === 4 || company.id === 5 ? 100 : 150}
-                  height={50} // Replace with actual dimensions
-                  className="md:w-24 w-20"
-                />
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+        {workExperience.map((card) => (
+          <Button
+            key={card.id}
+            //   random duration will be fun , I think , may be not
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            borderRadius="1.75rem"
+            style={{
+              //   add these two
+              //   you can generate the color from here https://cssgradient.io/
+              background: "rgb(4,7,29)",
+              backgroundColor:
+                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+              // add this border radius to make it more rounded so that the moving border is more realistic
+              borderRadius: `calc(1.75rem* 0.96)`,
+            }}
+            // remove bg-white dark:bg-slate-900
+            className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+              <img
+                src={card.thumbnail}
+                alt={card.thumbnail}
+                className="lg:w-32 md:w-20 w-16"
+              />
+              <div className="lg:ms-5">
+                <h1 className="text-start text-xl md:text-2xl font-bold">
+                  {card.title}
+                </h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {card.desc}
+                </p>
               </div>
-            </React.Fragment>
-          ))}
-        </div>
+            </div>
+          </Button>
+        ))}
       </div>
-    </section>
+    </div>
   );
 };
 
-export default Clients;
+export default Experience;
