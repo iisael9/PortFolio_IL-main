4729151838742956bb4cae6cd60d141fbbc3c86e
Isael <iisael090@gmail.com>
2025-06-08 18:45:26 -0700

    seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://port-folio-il-main.vercel.app/', // ← CHANGE this to your real domain
+    generateRobotsTxt: true, // Optional but recommended
+}
+  
